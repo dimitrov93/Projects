add8966752eb35b05c4b1e1f6d4851d004ba5481
dimitrov93 <ts.dimitrov93@gmail.com>
2022-12-05 11:13:32 +0200

    adding tile map

---
+++ javascript-pacman/v2/src/TileMap.js
@@ -0,0 +1,70 @@
+export default class TileMap {
+  constructor(tileSize) {
+    this.tileSize = tileSize;
+
+    this.yellowDot = new Image();
+    this.yellowDot.src = "images/yellowDot.png";
+
+    this.wall = new Image();
+    this.wall.src = "images/wall.png";
+ }
+
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 7, 0, 0, 4, 0, 0, 0, 0, 0, 0, 7, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 6, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 1, 7, 1, 1, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1],
+    [1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 1) {
+          this.#drawWall(ctx, column, row, this.tileSize);
+        } else if (tile === 0) {
+          this.#drawDot(ctx, column, row, this.tileSize);
+        }
+
+
+        // drawing the rows/colums
+        ctx.strokeStyle = 'yellow';
+        ctx.strokeRect(column * this.tileSize, row * this.tileSize, this.tileSize, this.tileSize)
+      }
+    }
+  }
+
+  #drawDot(ctx, column, row, size) {
+    ctx.drawImage(
+      this.yellowDot,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  #drawWall(ctx, column, row, size) {
+    ctx.drawImage(
+      this.wall,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
+
+
+}
